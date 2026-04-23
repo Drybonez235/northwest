36a8a46df471de8faf269602fe73a52385d6a8df
Jonathan Lewis <jonathan.lewis@welch.edu>
2026-04-23 14:23:49 -0500

    added the header to have css

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => '__root__',
         'pretty_version' => 'dev-main',
         'version' => 'dev-main',
-        'reference' => '38554760b40528072b88a19c8371337cdb46fd50',
+        'reference' => '5f97c06faa74095d6c91f4513eefb18883dd1c51',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -13,7 +13,7 @@
         '__root__' => array(
             'pretty_version' => 'dev-main',
             'version' => 'dev-main',
-            'reference' => '38554760b40528072b88a19c8371337cdb46fd50',
+            'reference' => '5f97c06faa74095d6c91f4513eefb18883dd1c51',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
