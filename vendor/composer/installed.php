6a82e9476dfec81eeaec9ef33579026c61e6b09d
Jonathan Lewis <jonathan.lewis@welch.edu>
2026-04-26 17:39:00 -0500

    added menu stuff

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => '__root__',
         'pretty_version' => 'dev-main',
         'version' => 'dev-main',
-        'reference' => '00782952584888f6321558545f8a2ea0832ef2bb',
+        'reference' => '8f518550bc18969aa098ac8f563409386c8f773a',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -13,7 +13,7 @@
         '__root__' => array(
             'pretty_version' => 'dev-main',
             'version' => 'dev-main',
-            'reference' => '00782952584888f6321558545f8a2ea0832ef2bb',
+            'reference' => '8f518550bc18969aa098ac8f563409386c8f773a',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
