c97d3d6a3ed8559f18c181da3875bd99b11f497e
Jonathan Lewis <jonathan.lewis@welch.edu>
2026-04-27 15:05:38 -0500

    added the code for a nav that displays ministries

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => '__root__',
         'pretty_version' => 'dev-main',
         'version' => 'dev-main',
-        'reference' => '6d32b32f5d7a0be7dc7adb2327bed60543c2233d',
+        'reference' => '72cfc1082a24200d8673f67630978e8919de1a68',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -13,7 +13,7 @@
         '__root__' => array(
             'pretty_version' => 'dev-main',
             'version' => 'dev-main',
-            'reference' => '6d32b32f5d7a0be7dc7adb2327bed60543c2233d',
+            'reference' => '72cfc1082a24200d8673f67630978e8919de1a68',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
