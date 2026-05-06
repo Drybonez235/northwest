7b3c4320ee6e3c781107a978d8653513ffd3193c
Jonathan Lewis <jonathan.lewis@welch.edu>
2026-05-06 11:49:25 -0500

    added a mobile menu

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => '__root__',
         'pretty_version' => 'dev-main',
         'version' => 'dev-main',
-        'reference' => 'c7d5459b7ef88191efeeef6c3ec8fe017de6104b',
+        'reference' => '440a83f34acbe72fbcdf080d22ef9ec54980f1c4',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -13,7 +13,7 @@
         '__root__' => array(
             'pretty_version' => 'dev-main',
             'version' => 'dev-main',
-            'reference' => 'c7d5459b7ef88191efeeef6c3ec8fe017de6104b',
+            'reference' => '440a83f34acbe72fbcdf080d22ef9ec54980f1c4',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
